403ad331bd0d2b487b3ee00b5c84f2d7c5845695
Helitha Rupasinghe <91548582+hr21don@users.noreply.github.com>
2022-04-20 11:00:40 +0100

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ require("dotenv").config();
 const server = http.createServer(app);
 const io = require("socket.io")(server, {
 	cors: {
-		origin: "*",
+		origin: "http://localhost:3000",
 		methods: [ "GET", "POST" ]
 	}
 });
